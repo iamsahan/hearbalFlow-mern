9e1a05da85d6226fdd5359621781fb6d5b240403
iamsahan <weerakkodyse@gmail.com>
2024-09-28 12:04:28 +0530

    change report name

--- frontend/src/Pages/InventoryDashboard.jsx
+++ frontend/src/Pages/InventoryDashboard.jsx
@@ -237,7 +237,7 @@ const InventoryDashboard = () => {
           />
           <PDFDownloadLink
             document={<InventoryReport packages={filteredItems} />}
-            fileName="filtered-packages.pdf"
+            fileName="inventory_report.pdf"
           >
             {({ loading }) => (
               <button
